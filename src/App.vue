c17229e65f9ca46cfa8a9c1a5a6285e24c02098c
Jason M Harrison <drinkingtheink@Jasons-MacBook-Air.local>
2021-08-24 22:03:52 -0500

    Added scss. Added data call to NASA, getting data.

--- src/App.vue
+++ src/App.vue
@@ -15,13 +15,17 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+@import './styles/palette';
+body {
+  background-color: $black;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/components/AppStage.vue
+++ src/components/AppStage.vue
@@ -1,8 +1,7 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h3>NEOWS Watch</h3>
-  </div>
+  <main>
+    <h1>{{ msg }} | NEOWS Watch</h1>
+  </main>
 </template>
 
 <script>
@@ -10,23 +9,51 @@ export default {
   name: 'AppStage',
   props: {
     msg: String
-  }
+  },
+  data() {
+    return {
+      apiUrl: 'https://api.nasa.gov/neo/rest/v1/feed?',
+      apiKey: 'Z1Y1LoyLfSDcQJI51fl8E1tYsrZQgnvU09C5pV36',
+    }
+  },
+  computed: {
+    today: function() {
+      return `2021-08-24`;
+    },
+    tomorrow: function() {
+      return '2021-08-25'
+    },
+    dataUrl: function() {
+      return `${this.apiUrl}start_date=${this.today}&api_key=${this.apiKey}`
+    }
+  },
+  mounted() {
+    this.getData();
+  },
+  methods: {
+    getData() {
+      fetch(this.dataUrl)
+        .then(response => {
+          console.info(`GOT A RESPONSE >>> ${JSON.stringify(response)}`);
+        })
+        .catch(error => {
+          console.error('There has been a problem with your fetch operation:', error);
+        });
+          }
+        }
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+@import '../styles/palette';
+
+h1 {
+  color: $yellow;
+}
+
 h3 {
   margin: 40px 0 0;
+  color: white;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
