b8f48d1fc969a644fe0a2f8139cd06e24f5852f0
drinkingtheink <drinkingtheink@Jasons-MacBook-Air.local>
2021-08-24 23:17:50 -0500

    Displaying bodies polish

--- src/components/ObjectDisplay.vue
+++ src/components/ObjectDisplay.vue
@@ -1,7 +1,12 @@
 <template>
     <div class="object-display-stage">
-        <div v-for="body in objectCollection" :key="body.neo_reference_id">
+        <div 
+            v-for="body in objectCollection" 
+            :key="body.neo_reference_id"
+            class="heavenly-body"
+        >
             <p>{{ body.name }}</p>
+            <span class="disc" />
         </div>
     </div>
 </template>
@@ -16,5 +21,18 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.object-display-stage {
+    position: relative;
+}
 
+$bodyDim: 20px;
+
+.disc {
+    border-radius: 50%;
+    height: $bodyDim;
+    width: $bodyDim;
+    background-color: white;
+    display: inline-block;
+    position: absolute;
+}
 </style>
