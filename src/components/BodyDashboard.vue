7bc62cbc5f84bd8495790db06f5d19a2cf308021
drinkingtheink <jason.m.harrison@gmail.com>
2021-08-25 13:28:06 -0500

    Body dispersion polish

--- src/components/BodyDashboard.vue
+++ src/components/BodyDashboard.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="body-dashboard-stage">
         <header>
-            <h1>Watchful Eye | NEOWS Watching {{ bodyCount }} objects today</h1>
+            <h1>Watchful Eye | NEOWS Watching <span class="count">{{ bodyCount }}</span> objects today</h1>
         </header>
         <Body 
             v-for="body, index in bodyCollection" 
             :key="body.neo_reference_id"
-            :style="{ 'right': `${getRandomInt(10, 90)}%`, 'top':  `${getDistanceFromEarth(body.close_approach_data[0].miss_distance)}%`, 'animation-delay': `0.${index}s`}"
+            :style="{ 'right': `${getRandomInt(10, 90)}%`, 'top': `${getDistanceFromEarth(body.close_approach_data[0].miss_distance)}%`, 'animation-delay': `0.${index}s`}"
             :getRandomInt="getRandomInt"
             :getDistanceFromEarth="getDistanceFromEarth"
             :body="body"
@@ -34,12 +34,39 @@ export default {
             return Math.floor(Math.random() * (max - min + 1)) + min;
         },
         getDistanceFromEarth(distData) {
-            let distance = distData.miles;
-            console.info(`DISTANCE: ${Math.floor(distance)}`);
+            let distance = Math.floor(distData.miles);
+            let bodyMin;
+            let bodyMax;
+            let floor = 1000000;
+            let medFloor = 150000;
+            let ceiling = 20000000;
+
+            if (distance < floor) {
+                bodyMin = 75;
+                bodyMax = 85;
+            }
+
+            else if (distance > floor && distance < medFloor) {
+                bodyMin = 55;
+                bodyMax = 70;
+            }
+
+            else if (distance > medFloor && distance < ceiling) {
+                bodyMin = 60;
+                bodyMax = 25;
+            }
+
+            else if (distance > ceiling) {
+                bodyMin = 15;
+                bodyMax = 25;
+            }
+
+            else {
+                bodyMin = 85;
+                bodyMax = 90;
+            }
             
-            // let distanceSetting;
-            
-            return this.getRandomInt(15, 90);
+            return this.getRandomInt(bodyMin, bodyMax);
         }
     }
 }
@@ -56,6 +83,10 @@ export default {
         justify-content: center;
         align-content: center;
         background-color: rgba(black, 0.4);
+    }
+
+    .count {
+        color: white;
     }
 }
 </style>
