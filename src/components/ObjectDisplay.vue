533ce8bb6f878094a991b05ff67310f3be622c1b
drinkingtheink <drinkingtheink@Jasons-MacBook-Air.local>
2021-08-24 23:10:09 -0500

    Starting to display bodies.

---
+++ src/components/ObjectDisplay.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="object-display-stage">
+        <div v-for="body in objectCollection" :key="body.neo_reference_id">
+            <p>{{ body.name }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ObjectDisplay',
+    props: {
+        objectCollection: Object
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
